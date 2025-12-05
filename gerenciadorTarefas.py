96572742da54905d90994055209c96d528db3cda
jftigre <joaofelipetigre@gmail.com>
2025-12-04 22:21:22 -0300

    feat: Implementa a funcionalidade de adicionar novas tarefas

---
+++ gerenciadorTarefas.py
@@ -0,0 +1,22 @@
+def adicionar_tarefas(lista_tarefas, nome_tarefa):
+    tarefa = {"tarefa": nome_tarefa, "completada": False}
+    lista_tarefas.append(tarefa)
+    print(f"Tarefa {nome_tarefa} foi adicionada com sucesso!")
+    return
+
+lista_tarefas = []
+
+while True:
+    print("\nMenu do Gerenciador de lista de tarefas:\n")
+    print("1. Adicionar tarefa")
+    print("2. Ver tarefas")
+    print("3. Atualizar tarefa")
+    print("4. Concluir tarefa")
+    print("5. Deletar tarefa")
+    print("6. Sair\n")
+
+    escolha = int(input("Digite a sua escolha: "))
+
+    if escolha == 1:
+        nome_tarefa = str(input("Digite o nome da tarefa: "))
+        adicionar_tarefas(lista_tarefas, nome_tarefa)
