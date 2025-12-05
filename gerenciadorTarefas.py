06d6eb5fb62cd1c0e5171d208fb61268989d52a7
jftigre <joaofelipetigre@gmail.com>
2025-12-04 22:29:08 -0300

    feat: Implementa a funcionalidade de atualizar tarefas

--- gerenciadorTarefas.py
+++ gerenciadorTarefas.py
@@ -12,6 +12,15 @@ def visualizar_tarefas(lista_tarefas):
         status = "[✓]" if tarefa["completada"] else "[ ]"
         print(f"{numero_tarefa}. {tarefa['tarefa']} {status}")
 
+def atualizar_tarefas(lista_tarefas, indice_tarefa, novo_nome_tarefa):
+    indice_tarefa -= 1
+    if indice_tarefa >= 0 and indice_tarefa < len(lista_tarefas):
+        lista_tarefas[indice_tarefa]["tarefa"] = novo_nome_tarefa
+        print(f"Tarefa {indice_tarefa+1} atualizada para {novo_nome_tarefa}")
+    else:
+        print("Índice de tarefa inválido!")
+    return
+
 
 lista_tarefas = []
 
@@ -33,3 +42,9 @@ while True:
     elif escolha == 2:
         visualizar_tarefas(lista_tarefas)
 
+    elif escolha == 3:
+        visualizar_tarefas(lista_tarefas)
+        indice_tarefa = int(input("Digite o número da tarefa que deseja atualizar: "))
+        novo_nome_tarefa = str(input("Digite o novo nome da tarefa: "))
+        atualizar_tarefas(lista_tarefas, indice_tarefa, novo_nome_tarefa)   
+    
